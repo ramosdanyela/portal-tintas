b5f7f546514fb59f30a681b24bea6cc26d75154e
Danyela Ramos <ramos.danyela@gmail.com>
2025-02-06 20:34:59 -0400

    hp responsiveness

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("614d4f392f76d43b")
+/******/ 		__webpack_require__.h = () => ("44bba4e1de0f88ef")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
